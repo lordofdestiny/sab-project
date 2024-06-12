894ffb8514f9a28340f321d354a5eb4e88344225
lod <djumicbranislav@outlook.com>
2024-06-12 12:51:51 +0200

    GeneralOperations implementation

---
+++ src/rs/etf/sab/student/db200260_GeneralOperations.java
@@ -0,0 +1,38 @@
+package rs.etf.sab.student;
+
+import rs.etf.sab.operations.GeneralOperations;
+import rs.etf.sab.student.util.DB;
+
+import java.sql.BatchUpdateException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class db200260_GeneralOperations implements GeneralOperations {
+
+    @Override
+    public void eraseAll() {
+        try {
+            Connection conn = DB.getInstance().getConnection();
+            try (final var stmt = conn.createStatement()) {
+                stmt.addBatch("DELETE FROM [RidePackages] WHERE 1 = 1");
+                stmt.addBatch("DELETE FROM [Ride] WHERE 1 = 1");
+                stmt.addBatch("DELETE FROM [CourierRequest]  WHERE 1 = 1");
+                stmt.addBatch("DELETE FROM [Offer] WHERE 1 = 1");
+                stmt.addBatch("DELETE FROM [Package] WHERE 1 = 1");
+                stmt.addBatch("DELETE FROM [District]  WHERE 1 = 1");
+                stmt.addBatch("DELETE FROM [City]  WHERE 1 = 1");
+                stmt.addBatch("DELETE FROM [Admin]  WHERE 1 = 1");
+                stmt.addBatch("DELETE FROM [Courier]  WHERE 1 = 1");
+                stmt.addBatch("DELETE FROM [Vehicle]  WHERE 1 = 1");
+                stmt.addBatch("DELETE FROM [User]  WHERE 1 = 1");
+                stmt.executeBatch();
+            }
+        } catch (SQLException e) {
+            if (e instanceof BatchUpdateException) {
+                System.out.println("One of the deletes violates referential integrity.");
+            }
+            e.printStackTrace();
+        }
+    }
+
+}
