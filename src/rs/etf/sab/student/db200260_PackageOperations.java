342a456d954aeeebac42002e139b5e0aa0d9b6e5
lod <djumicbranislav@outlook.com>
2024-06-12 22:24:50 +0200

    Implemented insertPackage for PackageOperations

--- src/rs/etf/sab/student/db200260_PackageOperations.java
+++ src/rs/etf/sab/student/db200260_PackageOperations.java
@@ -1,15 +1,43 @@
 package rs.etf.sab.student;
 
 import rs.etf.sab.operations.PackageOperations;
+import rs.etf.sab.student.util.DB;
 
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class db200260_PackageOperations implements PackageOperations {
     @Override
     public int insertPackage(int districtFrom, int districtTo, String userName, int packageType, BigDecimal weight) {
-        return 0;
+        final var connection = DB.getInstance().getConnection();
+        try (final var insertPackage = connection.prepareStatement(
+                "INSERT INTO [Package](IdDistFrom, IdDistTo, IdSender, Weight, PackageType)" +
+                        " VALUES (?, ?, (SELECT [IdUser] FROM [User] WHERE [Username] = ?), ?, ?)",
+                PreparedStatement.RETURN_GENERATED_KEYS
+        )) {
+            insertPackage.setInt(1, districtFrom);
+            insertPackage.setInt(2, districtTo);
+            insertPackage.setString(3, userName);
+            insertPackage.setBigDecimal(4, weight);
+            insertPackage.setInt(5, packageType);
+
+            if (insertPackage.executeUpdate() == 0) {
+                return -1;
+            }
+
+            try (final var keySet = insertPackage.getGeneratedKeys()) {
+                if (keySet.next()) {
+                    return keySet.getInt(1);
+                } else {
+                    return -1;
+                }
+            }
+        } catch (SQLException e) {
+            return -1;
+        }
     }
 
     @Override
@@ -38,7 +66,7 @@ public class db200260_PackageOperations implements PackageOperations {
     }
 
     @Override
-    public boolean changeWeight(int packageId, BigDecimal newWeight ) {
+    public boolean changeWeight(int packageId, BigDecimal newWeight) {
         return false;
     }
 
