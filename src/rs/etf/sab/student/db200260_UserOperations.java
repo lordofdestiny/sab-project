62181a59ad920911168e12f0d38442db6050c5a1
lod <djumicbranislav@outlook.com>
2024-06-12 21:12:00 +0200

    Implemented UserOperations

--- src/rs/etf/sab/student/db200260_UserOperations.java
+++ src/rs/etf/sab/student/db200260_UserOperations.java
@@ -1,31 +1,136 @@
 package rs.etf.sab.student;
 
+import rs.etf.sab.student.util.DB;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class db200260_UserOperations implements rs.etf.sab.operations.UserOperations {
 
     @Override
     public boolean insertUser(String userName, String firstName, String lastName, String password) {
-        return false;
+        final var connection = DB.getInstance().getConnection();
+        try (final var insertUser = connection.prepareStatement(
+                "INSERT INTO [User](FirstName, LastName, Username, Password) VALUES (?, ?, ?, ?)"
+        )) {
+            insertUser.setString(1, firstName);
+            insertUser.setString(2, lastName);
+            insertUser.setString(3, userName);
+            insertUser.setString(4, password);
+
+            return insertUser.executeUpdate() > 0;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    private boolean userExists(String username) {
+        final var connection = DB.getInstance().getConnection();
+        try (final var users = connection.prepareStatement(
+                "SELECT IdUser FROM [User] WHERE [Username] = ?"
+        )) {
+            users.setString(1, username);
+            try (final var resultSet = users.executeQuery()) {
+                return resultSet.next();
+            }
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    private boolean isAdmin(String username) {
+        final var connection = DB.getInstance().getConnection();
+        try (final var users = connection.prepareStatement(
+                "SELECT IdUser FROM [User] WHERE [Username] = ? AND IdUser IN (SELECT * FROM Admin)"
+        )) {
+            users.setString(1, username);
+            try (final var resultSet = users.executeQuery()) {
+                return resultSet.next();
+            }
+        } catch (SQLException e) {
+            return false;
+        }
     }
 
     @Override
     public int declareAdmin(String userName) {
-        return 0;
+        if (!userExists(userName)) {
+            return 2;
+        }
+        if (isAdmin(userName)) {
+            return 1;
+        }
+
+        final var connection = DB.getInstance().getConnection();
+        try (final var insertAdmin = connection.prepareStatement(
+                "INSERT INTO [Admin] VALUES ((SELECT IdUser FROM [User] WHERE [Username] = ?))"
+        )) {
+            insertAdmin.setString(1, userName);
+            final var result = insertAdmin.executeUpdate();
+            return result > 0 ? 0 : -1;
+        } catch (SQLException e) {
+            return -2;
+        }
     }
 
     @Override
     public Integer getSentPackages(String... userNames) {
-        return 0;
+        final var connection = DB.getInstance().getConnection();
+        try (final var userPackageCounts = connection.createStatement();
+             final var resultSet = userPackageCounts.executeQuery(
+                     "SELECT [Username], SentPackages FROM [User]"
+             )
+        ) {
+            var foundUsers = false;
+            var total = 0;
+            final var usernames = new HashSet<>(Arrays.asList(userNames));
+            while (resultSet.next()) {
+                final var username = resultSet.getString(1);
+                if (!usernames.contains(username)) continue;
+                total += resultSet.getInt(2);
+                foundUsers = true;
+            }
+            if (!foundUsers) {
+                return null;
+            }
+            return total;
+        } catch (SQLException e) {
+            return -1;
+        }
     }
 
     @Override
     public int deleteUsers(String... userNames) {
-        return 0;
+        final var connection = DB.getInstance().getConnection();
+        try (final var ps = connection.prepareStatement("DELETE FROM [User] WHERE [Username] = ?")) {
+            // noinspection Duplicates
+            for (var name : userNames) {
+                ps.setString(1, name);
+                ps.addBatch();
+            }
+            final var result = ps.executeBatch();
+            return Arrays.stream(result).filter(r -> r > 0).map(r -> 1).sum();
+        } catch (SQLException e) {
+            return -1;
+        }
     }
 
     @Override
     public List<String> getAllUsers() {
-        return List.of();
+        final var connection = DB.getInstance().getConnection();
+        try (final var stmt = connection.createStatement();
+             final var rs = stmt.executeQuery("SELECT [Username] FROM [User]")
+        ) {
+            final var users = new ArrayList<String>();
+            while (rs.next()) {
+                users.add(rs.getString(1));
+            }
+            return users;
+        } catch (SQLException e) {
+            return List.of();
+        }
     }
 }
